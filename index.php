051fdafbda7d887e5958e0cde0a6840c6bdd0cf8
Jan Trtík <jan.trtik@bonami.cz>
2016-11-25 23:02:44 +0100

    Scale simple stack with Traefic

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "Heja";
+$redis = new Redis();
+$redis->connect('redis');
+
+$hostname = getenv('HOSTNAME');
+
+$hits = $redis->incr('hits');
+$hitsByHostname = $redis->incr("hits:{$hostname}");
+
+?>
+<h1>Served by container: <?=$hostname ?></h1>
+<h3>Total hits: <?=$hits ?></h3>
+<h3>Hits on this container: <?=$hitsByHostname ?></h3>
